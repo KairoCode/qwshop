dbda3b360fa3446f4b84a159fd444b2c6dfe5372
1129759063@qq.com <1129759063@qq.com>
2025-06-17 11:08:37 +0800

    处理这个页面

--- resources/views/vue.blade.php
+++ resources/views/vue.blade.php
@@ -1,45 +1,52 @@
 <!doctype html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<html lang="{{ app()->getLocale() }}">
+
 <head>
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="csrf-token" content="{{ csrf_token() }}">
-    <title>{{ $web_name ?? '-' }}</title>
-    <meta name="keywords" content="{{ $keyword ?? '-' }}">
-    <meta name="description" content="{{ $description ?? '-' }}" />
+    <title>{{$web_name??'-'}}</title>
+    <meta name="keywords" content="{{$keyword??'-'}}">
+    <meta name="description" content="{{$description??'-'}}" />
 
-        <!-- 开发环境使用Vite加载 -->
-        @vite(['resources/js/app.js', 'resources/js/plugins/css/base.scss'])
-        <!-- 生产环境使用CDN -->
-        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/element-plus@2.3.12/dist/index.css" />
-        <script src="https://cdn.jsdelivr.net/npm/vue@3.3.4/dist/vue.global.min.js"></script>
-        <script src="https://cdn.jsdelivr.net/npm/vue-router@4.2.4/dist/vue-router.global.min.js"></script>
-        <script src="https://cdn.jsdelivr.net/npm/vuex@4.1.0/dist/vuex.global.min.js"></script>
-        <script src="https://cdn.jsdelivr.net/npm/element-plus@2.3.12/dist/index.full.min.js"></script>
-        <script src="https://cdn.jsdelivr.net/npm/nprogress@0.2.0/nprogress.js"></script>
-        <script src="https://cdn.jsdelivr.net/npm/dayjs@1.11.9/dayjs.min.js"></script>
-        <script src="https://cdn.jsdelivr.net/npm/xlsx@0.18.5/dist/xlsx.full.min.js"></script>
-        <script src="https://cdn.jsdelivr.net/npm/qrcode.vue@3.3.3/dist/qrcode.vue.global.min.js"></script>
-        <script src="https://cdn.jsdelivr.net/npm/clipboard@2.0.11/dist/clipboard.min.js"></script>
-        <script src="https://cdn.jsdelivr.net/npm/@antv/g2plot@2.4.20/dist/g2plot.min.js"></script>
-        <script src="https://cdn.jsdelivr.net/npm/@amap/amap-jsapi-loader@1.0.1/dist/index.js"></script>
+    <!-- <script src="https://cdn.jsdelivr.net/npm/vue-i18n@8.24.2/dist/vue-i18n.min.js"></script> 国际化 -->
 
-        <link href="https://cdn.jsdelivr.net/npm/font-awesome@4.7.0/css/font-awesome.min.css" rel="stylesheet" />
-        <link href="https://cdn.jsdelivr.net/npm/nprogress@0.2.0/nprogress.css" rel="stylesheet" />
+    <link rel="stylesheet" href="//unpkg.com/element-plus@2.3.12/dist/index.css" />
+    <script src="//unpkg.com/vue@3.3.4/dist/vue.global.prod.js"></script>
+    <script src="//unpkg.com/vue-router@4.2.4/dist/vue-router.global.prod.js"></script>
+    <script src="//unpkg.com/vuex@4.1.0/dist/vuex.global.prod.js"></script>
+    <script src="//unpkg.com/element-plus@2.3.12/dist/index.full.min.js"></script>
+    <script src="//unpkg.com/nprogress@0.2.0/nprogress.js"></script>
+    <script src="//unpkg.com/dayjs@1.11.9/dayjs.min.js"></script>
+    <script src="//unpkg.com/xlsx@0.18.5/dist/xlsx.core.min.js"></script> <!-- excel -->
+    <script src="//unpkg.com/qrcode.vue@3.3.3/dist/qrcode.vue.browser.min.js"></script> <!-- qrcode -->
+    <script src="//unpkg.com/clipboard@2.0.11/dist/clipboard.min.js"></script> <!-- clipboard -->
+    <script src="//unpkg.com/@antv/g2plot@2.4.20/dist/g2plot.min.js"></script> <!-- g2plot -->
+    <script src="//unpkg.com/@amap/amap-jsapi-loader@1.0.1/dist/index.js"></script> <!-- amap -->
 
-        <link href="https://cdn.jsdelivr.net/npm/@wangeditor/editor@5.1.23/dist/css/style.css" rel="stylesheet">
-        <script src="https://cdn.jsdelivr.net/npm/@wangeditor/editor@5.1.23/dist/index.min.js"></script>
+    <link href="//unpkg.com/font-awesome@4.7.0/css/font-awesome.css" rel="stylesheet" />
+    <link href="//unpkg.com/nprogress@0.2.0/nprogress.css" rel="stylesheet" /> <!-- 进度条 -->
 
-        <link href="{{ asset('dist/css/base.css') }}" rel="stylesheet" />
+    <!-- edit -->
+    <link href="//unpkg.com/@wangeditor/editor@5.1.23/dist/css/style.css" rel="stylesheet">
+    <script src="//unpkg.com/@wangeditor/editor@5.1.23/dist/index.js"></script>
+    <!-- edit -->
+
+
+    <link href="{{ mix('css/base.css','dist') }}" rel="stylesheet" />
+
+
 
 </head>
 
 <body>
-<div id="app">
-    <app></app>
-</div>
 
-        @vite('resources/js/app.js')
-    </body>
-</html>
+    <div id="app">
+        <app></app>
+    </div>
+
+
+</body>
+
+<script src="{{ mix('js/app.js','dist') }}"></script>
